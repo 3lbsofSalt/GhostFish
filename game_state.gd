5aa9aba7fddfa29cbca1650c1f52027fb8f00715
Isaac Peterson <isaacwilliam4@gmail.com>
2025-01-25 08:41:12 -0700

    added startup ui and restart/exit ui

--- game_state.gd
+++ game_state.gd
@@ -15,4 +15,4 @@ func is_running() -> bool:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/startupscene.tscn")
