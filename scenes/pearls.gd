776f0bfdf19f808121bf7f1bdd55b8e5e60adeeb
stocktonsmith <stocktonmichaelsmith@gmail.com>
2025-01-25 12:35:45 -0700

    Boss guy moves only when you're in the room with him

--- scenes/pearls.gd
+++ scenes/pearls.gd
@@ -1,5 +1,7 @@
 extends AnimatedSprite2D
 
+@onready var collected = false;
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,5 +16,7 @@ func _process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	# update sore
 	# change animation
-	if body.is_in_group('Fish'):
+	if body.is_in_group('Fish') and not collected:
+		collected = true;
 		self.play('collected')
+		$GetPearl.play();
