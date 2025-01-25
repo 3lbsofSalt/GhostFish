d049e6aa8c66f04597e4f64d16d31e792daf200b
3lbsofSalt <max.ramsdell@protonmail.com>
2025-01-25 15:29:31 -0700

    Added bubble pop

--- scenes/ghost_spawner.gd
+++ scenes/ghost_spawner.gd
@@ -16,7 +16,6 @@ func random_normal(mean: float, stddev: float) -> float:
 
 func _on_spawn_timer_timeout() -> void:
 	var rand = randf_range(0, 100);
-	print(GameState.enemies_should_spawn());
 	if rand <= spawn_chance and GameState.enemies_should_spawn():
 		var enemy: Node2D = spawn.instantiate();
 		enemy.global_position = global_position;
