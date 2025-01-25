1778b279c0cac21623cb4dd6227a1c3f0ee88c72
stocktonsmith <stocktonmichaelsmith@gmail.com>
2025-01-25 13:42:48 -0700

    Special backgrounds for all rooms

--- map.gd
+++ map.gd
@@ -57,6 +57,7 @@ func _process(delta: float) -> void:
 	if GameState.is_running() and GameState.pearls_collected == 3 and not threePearlsFound:
 		print("3 PEARLS!")
 		threePearlsFound = true
+		$boss_room.play("readyForBoss")
 	
 func convertTwoDimsToOne(row, col):
 	return SHIP_DIMENSION * row + col
