d3f84f4b0c501819ec3facfdf1ddf75051eecdc6
3lbsofSalt <max.ramsdell@protonmail.com>
2025-01-25 08:09:48 -0700

    Added lunge guy

---
+++ lunge_guy.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+@export var speed: float = 20;
+@export var health: float = 1;
+
+var alive = true;
+var player_target: Node2D = null;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func is_alive() -> bool:
+	return alive;
+
+func die() -> void:
+	alive = false;	
+	queue_free();
+
+func take_damage(damage = 1) -> void:
+	health -= damage;
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if health <= 0:
+		die();
+
+	var player = get_tree().get_nodes_in_group('Fish').pick_random();
+	if alive and GameState.is_running(): 
+		global_position = global_position.move_toward(player.global_position, delta*speed);
+
+
+func _on_collide(body: Node2D) -> void:
+	if body.is_in_group('Fish') and body.has_method('take_damage') and GameState.is_running():
+		body.take_damage();
+		take_damage();
