977c3a530a952ca3be1a1eba2699ab357ed26b2c
stocktonsmith <stocktonmichaelsmith@gmail.com>
2025-02-01 12:46:34 -0700

    Fixed find the boss text on new resolution

--- find_the_boss.gd
+++ find_the_boss.gd
@@ -2,6 +2,8 @@ extends Sprite2D
 
 @onready var boss_spawned = false
 
+@onready var SCREEN_WIDTH = get_viewport().get_visible_rect().size.x;
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,6 +15,6 @@ func _process(delta: float) -> void:
 	if GameState.pearls_collected >= 3 and not boss_spawned:
 		boss_spawned = true
 		var panTween = create_tween();
-		panTween.tween_property(self, "position", Vector2(576, position.y), .5)
-		panTween.tween_property(self, "position", Vector2(576, position.y), 1)
-		panTween.tween_property(self, "position", Vector2(2000, position.y), .5)
+		panTween.tween_property(self, "position", Vector2(SCREEN_WIDTH / 2, position.y), .5) # come from left
+		panTween.tween_property(self, "position", Vector2(SCREEN_WIDTH / 2, position.y), 1) # wait one second
+		panTween.tween_property(self, "position", Vector2(2000, position.y), .5) # leave to right
